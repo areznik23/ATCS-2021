9749f290e28f7dc610dafae5658586bcf52eae45
AlexReznik <alexreznik23@gmail.com>
2021-10-08 13:37:15 -0700

    While Loops and Input Exercises

--- project0/gamePref.py
+++ project0/gamePref.py
@@ -1,1 +1,6 @@
-# hello
+li = ['monopoly', 'risk', 'uno']
+print("I like the following games: monopoly, risk, uno")
+game = input('What is a game you like you: ')
+li.append(game)
+for item in li:
+    print(item)
--- project0/manyGames.py
+++ project0/manyGames.py
@@ -1,1 +1,9 @@
-#hello
+li = ['monopoly', 'risk', 'uno']
+print("I like the following games: monopoly, risk, uno")
+game = input('What is a game you like you: ')
+li.append(game)
+while(game != "no"):
+    game = input('What is a game you like (type no to stop): ')
+    li.append(game)
+for item in li:
+    print(item)
