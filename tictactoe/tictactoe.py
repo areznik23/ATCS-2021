84564d97aee22130be7b7fccb7a81422e9b885b4
AlexReznik <alexreznik23@gmail.com>
2022-02-17 22:36:57 -0800

    Alpha Beta

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -1,24 +1,17 @@
 from random import randint
+import time
 
-
-# narrow down the cylinder all the way
-# at a point as long as it is on a part of the cylinder that cancels
-# conceptualize the functions fully
-# take slow and understand each part of the problem
 class TicTacToe:
     def __init__(self):
-        # TODO: Set up the board to be '-'
         self.board = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
 
     def print_instructions(self):
-        # TODO: Print the instructions to the game
         print('Welcome to TicTacToe')
         print('Player 1 is X and Player 2 is O')
         print('Take turns placing your pieces - the first to 3 in a row wins!')
         return
 
     def print_board(self):
-        # TODO: Print the board
         print("  0 1 2")
         for i in range(len(self.board)):
             row = str(i) + " "
@@ -28,17 +21,13 @@ class TicTacToe:
         return
 
     def is_valid_move(self, row, col):
-        # TODO: Check if the move is valid
         return "-" == self.board[row][col]
 
     def place_player(self, player, row, col):
-        # TODO: Place the player on the board
         self.board[row][col] = player
         return
 
     def take_manual_turn(self, player):
-        # TODO: Ask the user for a row, col until a valid response
-        #  is given them place the player's icon in the right spot
         row = input('Enter a row: ')
         col = input('Enter a column: ')
         while not self.is_valid_move(int(row), int(col)):
@@ -59,9 +48,6 @@ class TicTacToe:
         return
 
     def minimax(self, player, depth):
-        # trying to limit the depth being searched, how to determine what the return values are
-        # depth equaling to zero should determine what the base case is
-
         opt_row = -1
         opt_col = -1
 
@@ -102,22 +88,72 @@ class TicTacToe:
                             worst = score
             return worst, opt_row, opt_col
 
+    # return from the function if alpha greater than equal to beta
+    def minimax_alpha_beta(self, player, depth, alpha, beta):
+        opt_row = -1
+        opt_col = -1
+
+        if self.check_win('O'):
+            return 10, None, None
+        if self.check_tie():
+            return 0, None, None
+        if self.check_win('X'):
+            return -10, None, None
+        if depth == 0:
+            return 0, None, None
+
+        if player == 'O':
+            best = -10
+            for i in range(len(self.board)):
+                for j in range(len(self.board[i])):
+                    if self.is_valid_move(i, j):
+                        self.place_player(player, i, j)
+                        score = self.minimax_alpha_beta('X', depth - 1, alpha, beta)[0]
+                        self.place_player('-', i, j)
+                        if best < score:
+                            opt_row = i
+                            opt_col = j
+                            best = score
+                        alpha = max(alpha, best)
+                        if alpha >= beta:
+                            break
+            return best, opt_row, opt_col
+
+        if player == 'X':
+            worst = 10
+            for i in range(len(self.board)):
+                for j in range(len(self.board[i])):
+                    if self.is_valid_move(i, j):
+                        self.place_player(player, i, j)
+                        score = self.minimax_alpha_beta('O', depth - 1, alpha, beta)[0]
+                        self.place_player('-', i, j)
+                        if worst > score:
+                            opt_row = i
+                            opt_col = j
+                            worst = score
+                        beta = min(beta, worst)
+                        if beta <= alpha:
+                            break
+            return worst, opt_row, opt_col
+
+    # currently using alpha beta minimax, can call minimax() for regular
     def take_minimax_turn(self, player, depth):
-        score, row, col = self.minimax(player, depth)
+        start = time.time()
+        score, row, col = self.minimax_alpha_beta(player, depth, -1000, 1000)
+        end = time.time()
+        print("This turn took:", end - start, "seconds")
         self.place_player(player, row, col)
 
     # NOTE: select depth > 0, else use take_random_turn()
     def take_turn(self, player):
-        # TODO: Simply call the take_manual_turn function
         print(player + "'s Turn")
         if player == 'X':
             self.take_manual_turn(player)
         if player == 'O':
-            depth = 1
+            depth = 10
             self.take_minimax_turn(player, depth)
         self.print_board()
 
-    # have to write all of the check win functions
     def check_col_win(self, player):
         for i in range(len(self.board)):
             col = []
@@ -131,7 +167,6 @@ class TicTacToe:
         return [player, player, player] in self.board
 
     def check_diag_win(self, player):
-        # TODO: Check diagonal win
         diags = [[self.board[0][0], self.board[1][1], self.board[2][2]],
                  [self.board[0][2], self.board[1][1], self.board[2][0]]]
         return [player, player, player] in diags
@@ -140,14 +175,12 @@ class TicTacToe:
         return self.check_row_win(player) or self.check_col_win(player) or self.check_diag_win(player)
 
     def check_tie(self):
-        # TODO: Check tie
         for i in range(len(self.board)):
             if '-' in self.board[i]:
                 return False
         return not self.check_win('X') and not self.check_win('O')
 
     def play_game(self):
-        # TODO: Play game
         self.print_instructions()
         self.print_board()
         player = 'X'
@@ -167,4 +200,3 @@ class TicTacToe:
         if self.check_tie():
             print('Tie Game')
             return
-        return
