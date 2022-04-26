d5c4c803014f2f23bcb044c287d33a91acbee811
AlexReznik <alexreznik23@gmail.com>
2022-04-26 13:39:22 -0700

    work on track retrieval for playlists

--- amped/main.py
+++ amped/main.py
@@ -1,9 +1,14 @@
-'''
-    Get list of the top 10,000 or so movies
-    Movie Lens Database to retrieve the information
-    Write a CSV file of all of those movies
-    Loop through the movies to pull the soundtrack playlists
-    Write the songs in the playlists to the CSV file
-    Loop through the songs for each movie and feed them into the spotify API
-    Get the features for each songs and write to the CSV file
-'''
+import requests as req
+
+from auth import access_token
+
+headers = {
+    'Authorization': 'Bearer {token}'.format(token=access_token)
+}
+
+r = req.get('https://api.spotify.com/v1/search?q=name:abc&type=playlist', headers=headers)
+r = r.json()
+new_id = r['playlists']['items'][1 ]['id']
+print(new_id)
+r = req.get('https://api.spotify.com/v1/playlists/' + new_id, headers=headers)
+print(r.json())
