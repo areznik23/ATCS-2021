a7f649667f2948f7f60407774a3f1f6c3d223f87
AlexReznik <alexreznik23@gmail.com>
2022-05-02 09:57:27 -0700

    playlists for movies from spotify api

--- amped/main.py
+++ amped/main.py
@@ -6,17 +6,27 @@ import urllib
 # generate a new csv document with all of the data, extract the titles
 
 from auth import access_token
+from retrieve_data import titles
 
 headers = {
     'Authorization': 'Bearer {token}'.format(token=access_token)
 }
+def get_request_function(title):
+    return title.replace(' ', '+')
 
-r = req.get('https://api.spotify.com/v1/search?q=Original+Motion+Picture+Soundtrack&type=playlist&limit=50', headers=headers)
-r = r.json()
-for item in r['playlists']['items']:
-    print(item['name'])
+for title in titles[:2]:
+    title = get_request_function(title)
+    r = req.get('https://api.spotify.com/v1/search?q='+title+'&type=playlist&limit=50', headers=headers)
+    id = r.json()['playlists']['items'][0]['id']
+    r = req.get('https://api.spotify.com/v1/playlists/' + id, headers=headers)
+    print(r.json())
 
-new_id = r['playlists']['items'][1]['id']
+# r = req.get('https://api.spotify.com/v1/search?q=Original+Motion+Picture+Soundtrack&type=playlist&limit=50', headers=headers)
+# r = r.json()
+# for item in r['playlists']['items']:
+#     print(item['name'])
+#
+# new_id = r['playlists']['items'][1]['id']
 
 # r = req.get('https://api.spotify.com/v1/playlists/' + new_id, headers=headers)
 # r = r.json()
